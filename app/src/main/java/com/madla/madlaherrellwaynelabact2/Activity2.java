9080dbbfdd7ef4c8c0d8d58f505ec8db10b5a10c
rellwayne <48982362+rellwayne@users.noreply.github.com>
2019-09-21 10:46:16 +0800

    Initial Commit

--- app/src/main/java/com/madla/madlaherrellwaynelabact2/Activity2.java
+++ app/src/main/java/com/madla/madlaherrellwaynelabact2/Activity2.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -11,9 +15,32 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        final EditText et9 = (EditText) findViewById(R.id.editText9);
+        Button button3 = (Button) findViewById(R.id.btnVerify);
+        button3.setOnClickListener (new View.OnClickListener(){
+            public void onClick(View v) {
+                SharedPreferences preferences = getSharedPreferences("PREF1", MODE_PRIVATE);
+                String s9 = et9.getText().toString().toLowerCase();
+                String ds1 = preferences.getString("p1",null);
+                String ds2 = preferences.getString("p2",null);
+                String ds3 = preferences.getString("p3",null);
+                String ds4 = preferences.getString("p4",null);
+                String ds5 = preferences.getString("p5",null);
+                String ds6 = preferences.getString("p6",null);
+                String ds7 = preferences.getString("p7",null);
+                String ds8 = preferences.getString("p8",null);
 
+                if (ds1.equals(s9) || ds2.equals(s9)|| ds3.equals(s9) || ds4.equals(s9) ||
+                        ds5.equals(s9) || ds6.equals(s9) || ds7.equals(s9) || ds8.equals(s9)){
+                    Toast.makeText(getApplicationContext(),"School is participating in UAAP.", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"School is not participating in UAAP.", Toast.LENGTH_LONG).show();
+                }
 
-        SharedPreferences preferences = getSharedPreferences("PREF1");
+            }
+        });
+
     }
 
 }
--- app/src/main/java/com/madla/madlaherrellwaynelabact2/MainActivity.java
+++ app/src/main/java/com/madla/madlaherrellwaynelabact2/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,25 +22,34 @@ public class MainActivity extends AppCompatActivity {
         final EditText et5 = (EditText) findViewById(R.id.editText5);
         final EditText et6 = (EditText) findViewById(R.id.editText6);
         final EditText et7 = (EditText) findViewById(R.id.editText7);
+        final EditText et8 = (EditText) findViewById(R.id.editText8);
 
         Button button2 = (Button) findViewById(R.id.btnSave);
         button2.setOnClickListener (new View.OnClickListener(){
             public void onClick(View v) {
                 SharedPreferences preferences = getSharedPreferences("PREF1", MODE_PRIVATE);
-                String s1 = et.getText().toString();
-                String s2 = et.getText().toString();
-                String s3 = et.getText().toString();
-                String s4 = et.getText().toString();
-                String s5 = et.getText().toString();
-                String s6 = et.getText().toString();
-                String s7 = et.getText().toString();
-                String s8 = et.getText().toString();
+                String s1 = et.getText().toString().toLowerCase();
+                String s2 = et2.getText().toString().toLowerCase();
+                String s3 = et3.getText().toString().toLowerCase();
+                String s4 = et4.getText().toString().toLowerCase();
+                String s5 = et5.getText().toString().toLowerCase();
+                String s6 = et6.getText().toString().toLowerCase();
+                String s7 = et7.getText().toString().toLowerCase();
+                String s8 = et8.getText().toString().toLowerCase();
 
                 SharedPreferences.Editor editor = preferences.edit();
 
-                editor.putString();
-                editor.commit();
+                editor.putString("p1", s1);
+                editor.putString("p2", s2);
+                editor.putString("p3", s3);
+                editor.putString("p4", s4);
+                editor.putString("p5", s5);
+                editor.putString("p6", s6);
+                editor.putString("p7", s7);
+                editor.putString("p8", s8);
 
+                editor.commit();
+                Toast.makeText(getApplicationContext(),"Data saved.", Toast.LENGTH_LONG).show();
             }
         });
 
